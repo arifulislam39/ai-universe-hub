449d130be929112b08929a9734516034d6890d7f
ariful-islam <arifulislam204909@gmail.com>
2023-03-02 22:53:13 +0600

    cards added

--- js/index.js
+++ js/index.js
@@ -6,7 +6,23 @@ const loadCards = async () => {
 }
 
 const displayCards = cards => {
-    console.log(cards)
+    CardsContainer = document.getElementById('cards-container');
+    cards.forEach(card => {
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('div');
+        cardDiv.innerHTML = `
+        <div class="card h-100">
+                        <img src="${card.image}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+        `;
+        CardsContainer.appendChild(cardDiv);
+        
+    });
 }
 
 loadCards();
